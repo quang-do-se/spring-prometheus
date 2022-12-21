3f66afb02cdfe63d075703a0ef55ab6d310a71f4
Quang Do <quang.do@colorado.edu>
2022-12-21 12:17:18 -0700

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import edu.colorado.oit.se.hashicorp.vault.utils.EchelonVault;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main implements ApplicationRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        SpringApplication.run(Main.class, args);
+	}
 
     @Override
     public void run(final ApplicationArguments args) throws Exception {
