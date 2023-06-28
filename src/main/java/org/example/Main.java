1197d258fdb0b8be59c143497d917f77dce3955f
Quang Do <quang.do@colorado.edu>
2023-06-28 04:41:01 -0600

    Test overlapping cron in Spring Schedule

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import static java.lang.Thread.sleep;
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,5 +20,12 @@ public class Main implements ApplicationRunner {
     @Override
     public void run(final ApplicationArguments args) {
         System.out.println("Hello");
+    }
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void overlappingSchedule() throws InterruptedException {
+        System.out.printf("%s - %s - Started %n", LocalDateTime.now(), Thread.currentThread().getName());
+        sleep(7000);
+        System.out.printf("%s - %s - Finished %n", LocalDateTime.now(), Thread.currentThread().getName());
     }
 }
