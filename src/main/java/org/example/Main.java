c9ec9471f6c0e137130cff1e3b6f34b3c0be0b16
Quang Do <quang.do@colorado.edu>
2024-01-02 10:20:07 -0700

    WIP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,16 @@
 package org.example;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties(TestProperties.class)
 public class Main implements ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
@@ -26,63 +18,11 @@ public class Main implements ApplicationRunner {
 
     @Override
     public void run(final ApplicationArguments args) {
-        System.out.println("Hello");
-        String tmpdir = System.getProperty("java.io.tmpdir");
-        System.out.println("Temp file path: " + tmpdir);
     }
 
-    @Autowired
-    TestProperties testProperties;
 
     @Scheduled(cron = "*/5 * * * * *")
     public void overlappingSchedule() throws IOException {
-        System.out.println(testProperties.getPassword());
-        createFileWithJavaIO();
-        createFileWithJavaNIO();
-        copyTempFileWithApacheIO();
-        copyNewFileWithApacheIO();
-    }
-
-    private void createFileWithJavaIO() {
-        try {
-            File myObj = new File("/data/newFileCreatedByJavaIO.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File IO created: " + myObj.getName());
-            }
-        } catch (IOException e) {
-            //
-        }
-    }
-
-    private void createFileWithJavaNIO() {
-        Path newFilePath = Paths.get("/data/newFileCreatedByJavaNIO.txt");
-        try {
-            Files.createFile(newFilePath);
-            System.out.println("File NIO created.");
-        } catch (IOException e) {
-            //
-        }
-    }
-
-    private void copyTempFileWithApacheIO() {
-        try {
-            File tempFile = File.createTempFile("tempFileCreatedByJavaIo", ".txt");
-            File newFile = new File("/data/tempFileCopiedByApacheIO.txt");
-            FileUtils.copyFile(tempFile, newFile);
-        } catch (IOException e) {
-            //
-        }
-    }
-
-    private void copyNewFileWithApacheIO () {
-        try {
-            Path newFile = Paths.get("/data/anotherFileCreatedByJavaNIO.txt");
-            Files.createFile(newFile);
-
-            File copyFile = new File("/data/newFileCopiedByApacheIO.txt");
-            FileUtils.copyFile(newFile.toFile(), copyFile);
-        } catch (IOException e) {
-            //
-        }
+        System.out.println("Hello");
     }
 }
